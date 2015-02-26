30ce01c4703a120e94f21bd9e2c7a87eb60cf8ff
Zoe <zoe.greene@tufts.edu>
2015-02-26 15:12:30 -0500

    added ms pacman and game board image slices

--- mspacman/game.js
+++ mspacman/game.js
@@ -3,11 +3,17 @@ function init(){
 
 var canvas = document.getElementById('game_canvas');
 var ctx = canvas.getContext('2d');
+var image = new Image();
+image.src = "pacman10-hp-sprite.png";
+
+image.addEventListener("load", function() {
 
 // Draw game board
-ctx.drawImage(pacman10-hp-spring.png, 322, 0, 464, 137, 0, 0, 464, 137);
+ctx.drawImage(image, 322, 0, 464, 137, 0, 0, 464, 137);
 
 // Draw Ms. Pacman
-//ctx.drawImage(pacman10-hp-spring.png, 83, 13, 13, 14, where on other image 
-	//should this appear? How do I remove whitespace? Which will overlap?)
+ctx.drawImage(image, 83, 23, 13, 14, 37, 35, 13, 14);
+	
+}, false);
+
 }
