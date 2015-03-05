0498d7c6fc1d906c083c4d0a1bf9ef37ebe8021e
Zoe <zoe.greene@tufts.edu>
2015-03-05 17:02:00 -0500

    finished part 1

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,20 @@
-// Your JavaScript goes here...
+function.parse(){
+	request = new XMLHttpRequest();
+	request.open("GET", "messages.json", true);
+	request.onreadystatechange = parseData;
+	request.send();
+}
+
+function.parseData(){
+	if (request.readyState == 4 && request.status == 200){
+		messagesDiv = document.getElementById("messages");
+		converted = JSON.parse(request.responseText);
+		for (i=0; i<converted.length; i++){
+			messagesDiv.innerHTML += "<p>" + converted[i]['content'] + " - " 
+			converted[i]['username'] + "</p>";
+		}
+	}
+	else if (request.readyState == 4 && request.status != 200){
+		alert("Page not loaded");
+	}
+}
