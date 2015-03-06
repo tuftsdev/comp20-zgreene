1d2e39ee84b26f3a8d3de4b8ea0339449a2af617
zgreen01 <zoe.greene@tufts.edu>
2015-03-06 16:10:01 -0500

    added username to print out

--- messages/lab.js
+++ messages/lab.js
@@ -10,7 +10,7 @@ function parseData(){
 		messagesDiv = document.getElementById("messages");
 		converted = JSON.parse(request.responseText);
 		for (i=0; i<converted.length; i++){
-			messagesDiv.innerHTML += "<p>" + converted[i]['content'] + " - " 
+			messagesDiv.innerHTML += "<p>" + converted[i]['content'] + " - " +
 			converted[i]['username'] + "</p>";
 		}
 	}
